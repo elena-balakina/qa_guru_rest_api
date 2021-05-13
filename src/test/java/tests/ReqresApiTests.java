86ebfa415b638f67a631454fdb921819ebe6b983
vera-ПК\vera <e.p.balakina@mail.ru>
2021-05-14 01:03:37 +0700

    Implement Get requests

---
+++ src/test/java/tests/ReqresApiTests.java
@@ -0,0 +1,55 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ReqresApiTests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @Test
+    public void successfulUsersLListTest() {
+        given().when()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("page", is(2))
+                .body("per_page", is(6))
+                .body("total", is(12))
+                .body("total_pages", is(2))
+                .body("data", not(nullValue()))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    public void successfulSingleUserTest() {
+        given().when()
+                .get("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"))
+                .body("data.avatar", is("https://reqres.in/img/faces/2-image.jpg"))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    public void unsuccessfulSingleUserTest() {
+        given().when()
+                .get("/api/users/23")
+                .then()
+                .statusCode(404)
+                .body("data", is(nullValue()));
+    }
+}
