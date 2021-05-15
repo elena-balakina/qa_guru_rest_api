4f6941a476f0ec412af873c70a99a8522e380c2f
vera-ПК\vera <e.p.balakina@mail.ru>
2021-05-15 19:40:37 +0700

    Added Registration and CRUD API tests

--- src/test/java/tests/ReqresApiTests.java
+++ src/test/java/tests/ReqresApiTests.java
@@ -5,6 +5,10 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -75,5 +79,84 @@ public class ReqresApiTests {
                 .then()
                 .statusCode(400)
                 .body("error", is("Missing password"));
+    }
+
+    @Test
+    public void successfulRegistrationTest() {
+        given().contentType(ContentType.JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\", " +
+                        "\"password\": \"pistol\" }")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    public void unsuccessfulRegistrationTest() {
+        given().contentType(ContentType.JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\"}")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    public void successfulSingleResourceTest() {
+        given().when()
+                .get("/api/unknown/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.name", is("fuchsia rose"))
+                .body("data.year", is(2001))
+                .body("data.color", is("#C74375"))
+                .body("data.pantone_value", is("17-2031"))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    public void successfulCreateUserTest() {
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+
+        given().contentType(ContentType.JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"))
+                .body("id",not(nullValue()))
+                .body("createdAt", containsString(currentDate));
+    }
+
+    @Test
+    public void successfulUpdateUserTest() {
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+
+        given().contentType(ContentType.JSON)
+                .body("{ \"name\": \"morpheus2\", \"job\": \"leader\" }")
+                .when()
+                .put("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus2"))
+                .body("job", is("leader"))
+                .body("updatedAt", containsString(currentDate));
+    }
+
+    @Test
+    public void successfulDeleteUserTest() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204);
     }
 }
